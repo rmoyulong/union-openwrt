4d30f7dcbe099e1163b4d405fc5b9a180285bb14
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-26 02:26:44 +0800

    通用设置 2024-05-26

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -49,6 +49,7 @@ sed -i 's/cn.pool.ntp.org/pool.ntp.org/' package/base-files/files/bin/config_gen
 sed -i 's/luci-theme-bootstrap/${ithemes}/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-argon/${ithemes}/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-design/${ithemes}/g' feeds/luci/collections/luci/Makefile
+rm -rf /feeds/luci/luci
 
 #固件版本号添加个人标识和日期
 [ -e package/lean/default-settings/files/zzz-default-settings ] && sed -i "s/DISTRIB_DESCRIPTION='.*OpenWrt '/DISTRIB_DESCRIPTION='莫小小($(TZ=UTC-8 date +%Y.%m.%d))@OpenWrt '/g" package/lean/default-settings/files/zzz-default-settings
