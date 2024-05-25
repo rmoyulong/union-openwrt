d1aec8e15a4d48f84d4727d3e8f7f3162ed9e1d4
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-25 23:15:36 +0800

    lede fullconenat  nftables firewall4 补丁

--- patch/nftables.sh
+++ patch/nftables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd openwrt
+
 function git_sparse_clone_base() {
   branch="$1" repourl="$2" && shift 2
   git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
