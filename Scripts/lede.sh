91faac160d3ed30b1da0b4044c0b6c2fc494ecfd
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-26 03:03:27 +0800

    删除冲突的插件

--- Scripts/lede.sh
+++ Scripts/lede.sh
@@ -25,7 +25,8 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-byp
 #######################################################################################
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages dnsforwarder
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede aria2
-#git_sparse_clone main https://github.com/kenzok8/small-package ariang
+rm -rf  package/openwrt-passwall/shadowsocks-rust
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocks-rust
 git_sparse_clone main https://github.com/kenzok8/small-package luci-app-kodexplorer
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages mosdns
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-mosdns
