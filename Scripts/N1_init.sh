d6c772e0b1f490a882e3bc3fd3e006b0150b6a79
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-26 16:23:40 +0800

    增加N1 lede 打包固件 2024-05-25

--- Scripts/N1_init.sh
+++ Scripts/N1_init.sh
@@ -1,14 +1,14 @@
-dos2unix $GITHUB_WORKSPACE/$OWRT_FILES/etc/*.sh
-dos2unix $GITHUB_WORKSPACE/$OWRT_FILES/etc/rc.*
-chmod -Rf 755 $GITHUB_WORKSPACE/$OWRT_FILES/etc/*.sh
-chmod -Rf 755 $GITHUB_WORKSPACE/$OWRT_FILES/etc/rc.*
-chmod -Rf 755 $GITHUB_WORKSPACE/$OWRT_FILES/etc/init.d/*
+dos2unix $1/$2/etc/*.sh
+dos2unix $1/$2/etc/rc.*
+chmod -Rf 755 $1/$2/etc/*.sh
+chmod -Rf 755 $1/$2/etc/rc.*
+chmod -Rf 755 $1/$2/etc/init.d/*
           
 #aria2
-chmod -R 777 $GITHUB_WORKSPACE/$OWRT_FILES/etc/aria2
-chmod -R 777 $GITHUB_WORKSPACE/$OWRT_FILES/mnt/sda1/aria2
-chmod -R 777 $GITHUB_WORKSPACE/$OWRT_FILES/mnt/sda1/aria2/download
-chmod -R 755 $GITHUB_WORKSPACE/$OWRT_FILES/mnt/sda1/share
+chmod -R 777 $1/$2/etc/aria2
+chmod -R 777 $1/$2/mnt/sda1/aria2
+chmod -R 777 $1/$2/mnt/sda1/aria2/download
+chmod -R 755 $1/$2/mnt/sda1/share
 		  
 sed -i 's/REENTRANT -D_GNU_SOURCE/LARGEFILE64_SOURCE/g' feeds/packages/lang/perl/perlmod.mk
 sed -i 's#GO_PKG_TARGET_VARS.*# #g' feeds/packages/utils/v2dat/Makefile
