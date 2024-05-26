5cfef0d22d5c26043d61d972914affa948fbd5ff
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-26 16:34:21 +0800

    openwrt 删除冲突的插件

---
+++ remove/openwrt_remove.sh
@@ -0,0 +1,19 @@
+#rm -rf feeds/packages/net/ztdns
+#rm -rf feeds/packages/net/zerotier
+#rm -rf feeds/packages/libs/libnatpmp
+#只有openwrt/luci没有feeds/luci/applications/luci-app-zerotier
+#openwrt/luci没有feeds/luci/applications/luci-app-kodexplorer
+#lede/luci没有feeds/luci/applications/luci-app-kodexplorer
+rm -rf feeds/packages/net/aria2
+rm -rf feeds/luci/applications/luci-app-zerotier
+rm -rf feeds/luci/applications/luci-app-turboacc
+rm -rf feeds/luci/applications/luci-app-kodexplorer
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/small/mosdns
+rm -rf feeds/small/luci-app-mosdns
+rm -rf feeds/small/shadowsocksr-libev
+rm -rf feeds/small/luci-app-ssr-plus
+rm -rf feeds/packages/net/homeproxy
+rm -rf feeds/luci/applications/luci-app-homeproxy
+rm -rf feeds/kenzo/homeproxy
+rm -rf feeds/kenzo/luci-app-homeproxy
