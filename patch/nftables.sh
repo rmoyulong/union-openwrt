4e25fc45fe0bbfb40285590259d928d085f9a739
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-27 17:15:46 +0800

    Lede nftables  firewall4 前后补丁

--- patch/nftables.sh
+++ patch/nftables.sh
@@ -14,14 +14,8 @@ cp -rf ./immortalwrt/package/libs/libnftnl package/libs/
 rm -rf ./package/network/utils/nftables/
 cp -rf ./immortalwrt/package/network/utils/nftables package/network/utils/
 
-# firewall
-rm -rf ./package/network/config/firewall
-cp -rf ./immortalwrt/package/network/config/firewall package/network/config/
-cp -rf $1/patch/nftables_Makefile package/network/config/firewall/Makefile
-
 # firewall4
 rm -rf ./package/network/config/firewall4
 cp -rf ./immortalwrt/package/network/config/firewall4 package/network/config/
-cp -rf $1/patch/nftables4_Makefile package/network/config/firewall4/Makefile
 
 rm -rf immortalwrt
