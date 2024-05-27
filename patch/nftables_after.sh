86a94a0b1ca12a087c021e79ec3c8cc9f1c4adc1
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-27 16:13:00 +0800

    Lede nftables 前后补丁

--- patch/nftables_after.sh
+++ patch/nftables_after.sh
@@ -14,6 +14,6 @@ function git_sparse_clone_base() {
 git clone -b master --depth 1 --single-branch https://github.com/immortalwrt/luci imm_luci
 # patch luci
 rm -rf feeds/luci/applications/luci-app-firewall
-cp -rf ./imm_luci/luci/applications/luci-app-firewall feeds/luci/applications/
+cp -rf ./imm_luci/applications/luci-app-firewall feeds/luci/applications/
 
 rm -rf imm_luci
