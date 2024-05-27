b81b1b700d0e6d752217bdb328b40d851f357b32
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-28 07:03:10 +0800

    lede 增加和删除扩展源

--- Scripts/lede.sh
+++ Scripts/lede.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
+#公用函数
+source $GITHUB_WORKSPACE/Scripts/functions.sh
+
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 ######################################################################################
