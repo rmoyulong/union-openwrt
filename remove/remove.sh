b188ee18f663142111e86e71a1e30e7f267549a1
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-29 01:45:56 +0800

    immortalwrt删除冲突的插件

--- remove/remove.sh
+++ remove/remove.sh
@@ -7,3 +7,18 @@ rm -rf feeds/kenzo/homeproxy
 rm -rf feeds/kenzo/luci-app-homeproxy
 ###############################
 rm -rf  package/openwrt-passwall/shadowsocks-rust
+####################################################
+#解决shadowsocks-libev编译错误
+git clone https://github.com/rmoyulong/AX6-Actions_Lede rmoyulong
+rm -rf feeds/packages/net/shadowsocks-libev
+cp -rf ./rmoyulong/shadowsocks-libev feeds/packages/net/
+
+rm -rf package/libs/mbedtls
+rm -rf package/libs/ustream-ssl
+rm -rf package/libs/uclient
+cp -rf ./rmoyulong/libs/mbedtls package/libs/
+cp -rf ./rmoyulong/libs/ustream-ssl package/libs/
+cp -rf ./rmoyulong/libs/uclient package/libs/
+
+rm -rf rmoyulong
+#####################################################
