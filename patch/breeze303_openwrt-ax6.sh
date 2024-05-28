8c26ed7257a6a5dc20d5d3c4ee4efe20a63bcc28
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-28 16:39:47 +0800

    breeze303_openwrt库编译ax6的补丁

--- patch/breeze303_openwrt-ax6.sh
+++ patch/breeze303_openwrt-ax6.sh
@@ -9,7 +9,14 @@ sudo mv $GITHUB_WORKSPACE/patch/ax6/11-ath10k-caldata target/linux/qualcommax/ip
 sudo mv $GITHUB_WORKSPACE/patch/ax6/11-ath11k-caldata target/linux/qualcommax/ipq807x/base-files/etc/hotplug.d/firmware/11-ath11k-caldata
 sudo mv $GITHUB_WORKSPACE/patch/ax6/bootcount target/linux/qualcommax/ipq807x/base-files/etc/init.d/bootcount
 sudo mv $GITHUB_WORKSPACE/patch/ax6/platform.sh target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
-sudo mv $GITHUB_WORKSPACE/patch/breeze303_ipq807x.mk target/linux/qualcommax/image/ipq807x.mk 
+sudo mv $GITHUB_WORKSPACE/patch/breeze303/breeze303_ipq807x.mk target/linux/qualcommax/image/ipq807x.mk 
 sudo mv $GITHUB_WORKSPACE/patch/ax6/qualcommax_ipq807x package/boot/uboot-envtools/files/qualcommax_ipq807x
 sudo mv $GITHUB_WORKSPACE/patch/ax6/ipq8071-ax6-stock.dts target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq8071-ax6-stock.dts
 sudo mv $GITHUB_WORKSPACE/patch/ax6/ipq8071-ax3600-stock.dts target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/ipq8071-ax3600-stock.dts
+
+#luci补丁
+git config --global user.name "OpenWrt Builder"
+git config --global user.email "buster-openwrt@ovvo.uk"
+cp $GITHUB_WORKSPACE/patch/breeze303/0001-show-soc-status-on-luci.patch feeds/luci
+cd feeds/luci
+git am 0001-show-soc-status-on-luci.patch
