bd28dc11a89eb504583b823810ebd99165901249
GitHub <noreply@github.com>
2024-05-30 00:20:42 +0800

    增加章鱼星球-openwrt 打包固件 2024-05-30

---
+++ Scripts/zyxq_init.sh
@@ -0,0 +1,14 @@
+dos2unix ${GITHUB_WORKSPACE}/$1/etc/*.sh
+dos2unix ${GITHUB_WORKSPACE}/$1/etc/rc.*
+chmod -Rf 755 ${GITHUB_WORKSPACE}/$1/etc/*.sh
+chmod -Rf 755 ${GITHUB_WORKSPACE}/$1/etc/rc.*
+chmod -Rf 755 ${GITHUB_WORKSPACE}/$1/etc/init.d/*
+          
+#aria2
+chmod -R 777 ${GITHUB_WORKSPACE}/$1/etc/aria2
+chmod -R 777 ${GITHUB_WORKSPACE}/$1/mnt/sda1/aria2
+chmod -R 777 ${GITHUB_WORKSPACE}/$1/mnt/sda1/aria2/download
+chmod -R 755 ${GITHUB_WORKSPACE}/$1/mnt/sda1/share
+		  
+sed -i 's/REENTRANT -D_GNU_SOURCE/LARGEFILE64_SOURCE/g' feeds/packages/lang/perl/perlmod.mk
+sed -i 's#GO_PKG_TARGET_VARS.*# #g' feeds/packages/utils/v2dat/Makefile
