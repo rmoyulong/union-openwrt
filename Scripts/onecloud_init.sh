dcfb53828da5d8d7c851f9e180574fbcf66d2190
GitHub <noreply@github.com>
2024-05-30 01:53:14 +0800

    增加玩客云-immortalwrt 打包固件 2024-05-30

---
+++ Scripts/onecloud_init.sh
@@ -0,0 +1,31 @@
+#############################################
+###拷贝meson amlogic soc架构
+git clone https://github.com/rmoyulong/OneCloud_OpenWrt OneCloud
+if [[ "$2" == *"onecloud-amlogic.config"* ]]; then
+  mv OneCloud/lede6.6/target/linux/amlogic target/linux/amlogic
+  chmod -Rf 755 target/linux/amlogic
+else
+  mv OneCloud/lede6.6/target/linux/meson target/linux/meson
+  chmod -Rf 755 target/linux/meson
+fi
+
+#如果files文件夹不存在，创建文件夹
+if [ ! -d "./files" ]; then
+  mkdir ./files
+fi
+
+cp -rf OneCloud/onecloud/files/* ./files
+rm -rm OneCloud
+##############################################
+
+dos2unix ./files/etc/*.sh
+dos2unix ./files/etc/rc.*
+chmod -Rf 755 ./files/etc/*.sh
+chmod -Rf 755 ./files/etc/rc.*
+chmod -Rf 755 ./files/etc/init.d/*
+          
+#aria2
+chmod -R 777 ./files/etc/aria2
+chmod -R 777 ./files/mnt/sda1/aria2
+chmod -R 777 ./files/mnt/sda1/aria2/download
+chmod -R 755 ./files/mnt/sda1/share
