2b5ec3a39ad34cb90b751839e988825cd448e502
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-30 19:00:50 +0800

    aria2 可道云 zerotier homeproxy启动 2024-05-30

--- patch/zyxq/files/etc/1.sh
+++ patch/zyxq/files/etc/1.sh
@@ -9,7 +9,8 @@ echo "# Put your custom commands here that should be executed once
 #mount -o remount rw /
 /usr/sbin/balethirq.pl
 sh /etc/diymo.sh
-#aria2c --conf-path=/etc/aria2/aria2.conf.main >/dev/null 2>&1
+aria2c --conf-path=/etc/aria2/aria2.conf.main >/dev/null 2>&1
 exit 0">/etc/rc.local
+
 rm -rf /etc/1.sh
 sh /etc/rc.local
