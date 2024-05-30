d439ed2bf0b8077e44fb0667e0285a5ca65b1807
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-05-30 10:34:22 +0800

    章鱼星球初始化设置

--- Scripts/zyxq_init.sh
+++ Scripts/zyxq_init.sh
@@ -9,7 +9,3 @@ chmod -R 777 ${GITHUB_WORKSPACE}/$1/etc/aria2
 chmod -R 777 ${GITHUB_WORKSPACE}/$1/mnt/sda1/aria2
 chmod -R 777 ${GITHUB_WORKSPACE}/$1/mnt/sda1/aria2/download
 chmod -R 755 ${GITHUB_WORKSPACE}/$1/mnt/sda1/share
-
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-sed -i 's/REENTRANT -D_GNU_SOURCE/LARGEFILE64_SOURCE/g' feeds/packages/lang/perl/perlmod.mk
