d7d6944551db54d8c0964d51c93c6d717106b21b
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-01 19:52:38 +0800

    openwrt增加和删除扩展源

--- Scripts/openwrt.sh
+++ Scripts/openwrt.sh
@@ -18,6 +18,7 @@ git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-hom
 #git_sparse_clone main https://github.com/xiaorouji/openwrt-passwall2 openwrt-passwall2
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede pcre
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede aria2
+git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede luci-app-overwall
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages dnsforwarder
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages mosdns
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-mosdns
