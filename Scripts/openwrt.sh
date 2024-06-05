be4ce47019cbd0792312d3ae8eb83fd4e4a15092
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-05 23:38:03 +0800

    openwrt增加和删除扩展源

--- Scripts/openwrt.sh
+++ Scripts/openwrt.sh
@@ -2,6 +2,3 @@
 
 sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/union-openwrt/releases/download/union_package/openwrt.tar.gz
 tar zxvfp rmoyulong.tar.gz
-
-# 取消主题默认设置
-find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
