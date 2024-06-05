a8a51cf0fa411995f8ceb9791a087d06e6cbdce9
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-06 00:36:16 +0800

    lede 增加和删除扩展源

--- Scripts/lede.sh
+++ Scripts/lede.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -rf package/libs/mbedtls
+rm -rf package/libs/ustream-ssl
+rm -rf package/libs/uclient
+
 sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/union-openwrt/releases/download/union_package/lede.tar.gz
 tar zxvfp rmoyulong.tar.gz
 
