6d6e9556aadb24f55168654867f4245f10ac0ee1
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-07 11:15:34 +0800

    immortalwrt 增加和删除扩展源

--- Scripts/immortalwrt.sh
+++ Scripts/immortalwrt.sh
@@ -3,6 +3,7 @@
 rm -rf package/libs/mbedtls
 rm -rf package/libs/ustream-ssl
 rm -rf package/libs/uclient
+rm -rf package/network/services/hostapd
 
 sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/union-openwrt/releases/download/union_package/immortalwrt.tar.gz
 tar zxvfp rmoyulong.tar.gz
