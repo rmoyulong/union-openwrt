efc0004f7f315fef4c8aabcf10a63afa73e82547
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-08 19:05:58 +0800

    armv7 32位 openclash

--- Scripts/openclash32.sh
+++ Scripts/openclash32.sh
@@ -1,5 +1,10 @@
 #设置armv7 32位的openclash
 
+meta="openclash"
+if [ ! -z "$1" ];then
+    meta=$1
+fi
+
 #删除以前设置的所有openclash
 rm -rf ./package/OpenClash
 rm -rf ./package/luci-app-openclash
@@ -16,7 +21,12 @@ git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/
 
 CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-armv7.tar.gz"
 CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep armv7 | awk -F '"' '{print $4}')
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-armv7.tar.gz"
+
+if [ "${meta}" == *"openclash"* ];then
+  CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-armv7.tar.gz"
+else
+  CLASH_META_URL="https://github.com/DustinWin/clash_singbox-tools/raw/main/mihomo-meta/mihomo-linux-armv7.tar.gz"
+fi
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 GEO_MMDB_URL="https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb"
