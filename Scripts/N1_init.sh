6f59a85f5de6af4d1eff7776ad6fb176c9baa603
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-10 21:31:44 +0800

    lede - N1【armv8】init 初始化设置

--- Scripts/N1_init.sh
+++ Scripts/N1_init.sh
@@ -1,15 +1,30 @@
-dos2unix ${GITHUB_WORKSPACE}/$1/etc/*.sh
-dos2unix ${GITHUB_WORKSPACE}/$1/etc/rc.*
-chmod -Rf 755 ${GITHUB_WORKSPACE}/$1/etc/*.sh
-chmod -Rf 755 ${GITHUB_WORKSPACE}/$1/etc/rc.*
-chmod -Rf 755 ${GITHUB_WORKSPACE}/$1/etc/init.d/*
+#############################################
+###拷贝斐讯N1配置
+
+git clone https://github.com/rmoyulong/AX6-Actions_Lede AX6-Actions_Lede
+
+#如果files文件夹不存在，创建文件夹
+if [ ! -d "./files" ]; then
+  mkdir ./files
+fi
+
+cp -rf AX6-Actions_Lede/union_files/$1/* ./files
+rm -rf AX6-Actions_Lede
+##############################################
+
+dos2unix ./files/etc/*.sh
+dos2unix ./files/etc/rc.*
+chmod -Rf 755 ./files/etc/*.sh
+chmod -Rf 755 ./files/etc/rc.*
+chmod -Rf 755 ./files/etc/init.d/*
           
 #aria2
-chmod -R 777 ${GITHUB_WORKSPACE}/$1/etc/aria2
-chmod -R 777 ${GITHUB_WORKSPACE}/$1/mnt/sda1/aria2
-chmod -R 777 ${GITHUB_WORKSPACE}/$1/mnt/sda1/aria2/download
-chmod -R 755 ${GITHUB_WORKSPACE}/$1/mnt/sda1/share
-		  
+chmod -R 777 ./files/etc/aria2
+chmod -R 777 ./files/mnt/sda1/aria2
+chmod -R 777 ./files/mnt/sda1/aria2/download
+chmod -R 755 ./files/mnt/sda1/share
+
+############################################################################################		  
 sed -i 's/REENTRANT -D_GNU_SOURCE/LARGEFILE64_SOURCE/g' feeds/packages/lang/perl/perlmod.mk
 sed -i 's#GO_PKG_TARGET_VARS.*# #g' feeds/packages/utils/v2dat/Makefile
 			
