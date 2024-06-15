3950fd50185f54217a1b12497c6a896ad6e075ef
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-15 17:51:31 +0800

    删除不需要的设置

--- Scripts/immortalwrt-nss.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf package/libs/mbedtls
-rm -rf package/libs/ustream-ssl
-rm -rf package/libs/uclient
-
-sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/union-openwrt/releases/download/union_package/immortalwrt-nss.tar.gz
-tar zxvfp rmoyulong.tar.gz
