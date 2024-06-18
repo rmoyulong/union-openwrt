da92dc15bc29895144589c1676c114e06164d257
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2024-06-19 05:15:50 +0800

    lede使用自定义nss 补丁 删除lede -qca [+turboacc]

--- patch/before-add-config-patch.sh
+++ patch/before-add-config-patch.sh
@@ -6,13 +6,16 @@ cd openwrt
 rm -rf ./package/qca
 
 #添加nss 附加依赖
-# AgustinLorenzo/nss-packages
+# AgustinLorenzo/openwrt 自带qca-nss-dp
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede qca-nss-dp
 
-#用lede替换AgustinLorenzo/nss-packages相同的插件
+#用lede替换AgustinLorenzo/nss-packages相同的插件package/qca/qca-nss-ecm package/qca/qca-ssdk
 rm -rf feeds/nss_packages/qca-nss-ecm
 rm -rf feeds/nss_packages/qca-ssdk
 git_sparse_clone master https://github.com/rmoyulong/AX6-Actions_Lede qca
+cp -rf ./package/qca/qca-nss-ecm feeds/nss_packages
+cp -rf ./package/qca/qca-ssdk ./package/qca-ssdk
+rm -rf ./package/qca
 
 #增加turboacc
 echo -e 'luci-app-turboacc=y' >> .config
