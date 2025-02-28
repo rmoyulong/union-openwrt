058616d575f7118f1271cd18f3f55148d6fd7b9a
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-02-28 13:29:36 +0800

    公用函数

--- Scripts/functions.sh
+++ Scripts/functions.sh
@@ -32,13 +32,23 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
+# Git稀疏克隆，只克隆指定目录到本地 ./base目录
+function git_sparse_clone_base_1() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../base
+  cd .. && rm -rf $repodir
+}
+
 # Git稀疏克隆，只克隆指定目录到本地 ./ 目录
 function git_sparse_clone_base() {
   branch="$1" repourl="$2" && shift 2
   git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
   repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
   cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../base
+  mv -f $@ ../
   cd .. && rm -rf $repodir
 }
 
