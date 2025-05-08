b2e48574dae34c92cad839c8f6a37ec2be2ab958
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-05-08 12:19:48 +0800

    test

---
+++ soc/testop.sh
@@ -0,0 +1,5 @@
+rm -r op
+mkdir -p  /home/moyulong/op/openwrt/bin/targets/*/*/
+sudo cp /home/moyulong/*rootfs.tar.gz  /home/moyulong/op/openwrt/bin/targets/*/*/
+sudo cp /home/moyulong/convert3.sh  /home/moyulong/op/
+chmod -Rf 755  /home/moyulong/op
