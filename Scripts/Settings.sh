6896699a4cc04515d762323ebcea2636232c13cb
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-05-15 14:18:19 +0800

    通用设置

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source $GITHUB_WORKSPACE/Scripts/functions.sh
 ip="192.168.31.1"
 iname="Openwrt"
 ithemes="luci-theme-material"
@@ -16,12 +15,10 @@ fi
 #修改默认主题
 #替换主题为原版argon
 if [[ "$4" == *"lede"* ]]; then
-
-  if [[ "$5" == *"x86_64"* ]]; then
-    rm -rf feeds/luci/themes/luci-theme-argon && git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-  else
-    rm -rf feeds/luci/themes/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-  fi
+  rm -rf feeds/luci/themes/luci-theme-argon
+  rm -rf package/luci-theme-argon
+  git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+  
   #修改lede默认时间格式
   sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
 fi
