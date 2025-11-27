4f596de4cfac82a12b9cd1dccab5cc0daca40724
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-11-28 00:44:00 +0800

    使用旧版的N1 & 章鱼 armsr

---
+++ patch/armsr_python3_patch.sh
@@ -0,0 +1,11 @@
+#公用函数
+source $GITHUB_WORKSPACE/Scripts/functions.sh
+
+cd openwrt
+rm -rf feeds/packages/lang/python
+
+merge_package master https://github.com/rmoyulong/old_coolsnowwolf_packages feeds/packages/lang lang/python
+
+rm -rf target/linux/armsr
+tar zcvfp $GITHUB_WORKSPACE/patch/armsr.tar.gz
+chmod -Rf 755 target/linux/armsr
