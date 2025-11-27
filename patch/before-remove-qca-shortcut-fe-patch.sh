b4a3c737d80f9f3ec3547b28ea6004d61d2a0e8b
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-11-27 22:42:17 +0800

    使用自定义turboacc,必须删除lede自带的shortcut-fe

--- patch/before-remove-qca-shortcut-fe-patch.sh
+++ patch/before-remove-qca-shortcut-fe-patch.sh
@@ -1,2 +1,6 @@
 #如果使用自定义的nss wifi插件就要删除lede自带的qca
 rm -rf openwrt/package/qca/shortcut-fe
+
+# 调整内核版本为 5.15
+sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=5.15/' openwrt/target/linux/amlogic/Makefile
+			
