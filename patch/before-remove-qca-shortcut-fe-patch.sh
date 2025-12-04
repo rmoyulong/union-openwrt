8c492134caaf91bbcc342a2a519c6b24b2396b02
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2025-12-04 15:03:43 +0800

    使用自定义turboacc,必须删除lede自带的shortcut-fe 经典amlogic

--- patch/before-remove-qca-shortcut-fe-patch.sh
+++ patch/before-remove-qca-shortcut-fe-patch.sh
@@ -8,10 +8,12 @@ rm -rf openwrt/package/qca/shortcut-fe
 cd openwrt
 rm -rf target/linux/amlogic
 tar -xzvf $GITHUB_WORKSPACE/patch/amlogic.tar.gz
-chmod -Rf 755 target/linux/amlogic
+#chmod -Rf 755 target/linux/amlogic
+
+rm -rf feeds/packages/lang/python
+merge_package master https://github.com/rmoyulong/old_coolsnowwolf_packages feeds/packages/lang lang/python
 
 # 调整内核版本为 5.15
 sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=5.15/' target/linux/amlogic/Makefile
-			
-rm -rf feeds/packages/lang/python
-merge_package master https://github.com/rmoyulong/old_coolsnowwolf_packages feeds/packages/lang lang/python
+
+cat target/linux/amlogic/Makefile
