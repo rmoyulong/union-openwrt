53e10c6cd97626b5dfeff7d42b42d16595429dd9
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-03-05 22:55:49 +0800

    增加和删除扩展源

---
+++ Scripts/setup.sh
@@ -0,0 +1,19 @@
+#公用函数
+source $GITHUB_WORKSPACE/Scripts/functions.sh
+
+merge_package master https://github.com/coolsnowwolf/packages package lang/rust
+git clone https://github.com/stupidloud/helloworld package/helloworld
+
+rm -rf package/helloworld/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+
+rm -rf package/helloworld/shadowsocks-rust
+merge_package master https://github.com/kenzok8/small/ package shadowsocks-rust
+
+rm -rf package/helloworld/xray-core
+git_sparse_clone main https://github.com/Openwrt-Passwall/openwrt-passwall-packages xray-core
+
+cd package
+$GITHUB_WORKSPACE/Scripts/Packages.sh
+$GITHUB_WORKSPACE/Scripts/Handles.sh
