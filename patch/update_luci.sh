df70abb5355c0a6b8912ec11fbdf5eba888dde19
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-08-30 23:46:25 +0800

    Add files via upload

--- patch/update_luci.sh
+++ patch/update_luci.sh
@@ -2,7 +2,13 @@
 
 cd openwrt
 ./scripts/feeds update -a
-  
+
+rm -rf package/feeds/packages/containerd/*
+rm -rf feeds/packages/utils/containerd/*
+
+cp -rf $GITHUB_WORKSPACE/patch/containerd/* package/feeds/packages/containerd/
+cp -rf $GITHUB_WORKSPACE/patch/containerd/* feeds/packages/utils/containerd/
+
 # Adjust source code
 patch -p1 -f < $(dirname "$0")/luci.patch
 
