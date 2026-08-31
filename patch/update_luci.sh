670cea24d70cd915e43b0e7b123119147c8785e9
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-08-31 10:47:06 +0800

    Add files via upload

--- patch/update_luci.sh
+++ patch/update_luci.sh
@@ -3,14 +3,13 @@
 cd openwrt
 ./scripts/feeds update -a
 
-rm -rf package/feeds/packages/containerd/*
+git clone --depth=1 https://github.com/immortalwrt/packages packages_temp
+
+rm -rf feeds/packages/utils/dockerd/*
 rm -rf feeds/packages/utils/containerd/*
 
-cp -rf $GITHUB_WORKSPACE/patch/containerd/* package/feeds/packages/containerd/
-cp -rf $GITHUB_WORKSPACE/patch/containerd/* feeds/packages/utils/containerd/
+cp -rf ./packages_temp/utils/dockerd/* feeds/packages/utils/dockerd/
+cp -rf ./packages_temp/utils/containerd/* feeds/packages/utils/containerd/
 
 # Adjust source code
 patch -p1 -f < $(dirname "$0")/luci.patch
-
-
-
