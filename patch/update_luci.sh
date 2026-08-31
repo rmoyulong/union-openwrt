eca868fc5ea61483388085a53e6595389ae5d047
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-08-31 18:18:12 +0800

    Add files via upload

--- patch/update_luci.sh
+++ patch/update_luci.sh
@@ -4,6 +4,7 @@ sudo mv /usr/local/bin/runc /usr/local/bin/runc.bak
 
 cd openwrt
 ./scripts/feeds update -a
+./scripts/feeds install -a
 
 git clone --single-branch --branch master https://github.com/immortalwrt/packages packages_temp
 
@@ -13,8 +14,8 @@ git clone --single-branch --branch master https://github.com/immortalwrt/package
 #cp -rf ./packages_temp/utils/dockerd/* feeds/packages/utils/dockerd/
 #cp -rf ./packages_temp/utils/containerd/* feeds/packages/utils/containerd/
 
-rm -rf feeds/packages/*
-cp -rf ./packages_temp/* feeds/packages/
+rm -rf feeds/packages/utils/*
+cp -rf ./packages_temp/utils/* feeds/packages/utils/
 
 # Adjust source code
 patch -p1 -f < $(dirname "$0")/luci.patch
